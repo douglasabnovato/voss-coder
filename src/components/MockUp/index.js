65d6f2cf4006552da13150f1c2e8b514d38621a6
Douglas A B Novato <douglasabnovato@gmail.com>
2022-12-06 14:48:49 -0300

    elemento mockup

--- src/components/MockUp/index.js
+++ src/components/MockUp/index.js
@@ -1,9 +1,24 @@
-function MockUp(){
-    return (
-        <div>
-            MockUp
-        </div>
-    )
+import React, { useState, useEffect } from "react";
+import FilterableProductTable from "./FilterableProductTable";
+
+function MockUp() {
+  const [products, setProducts] = useState([]);
+
+  function getData() {
+    fetch("./dataProducts.json", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => setProducts(res.dataProducts));
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <FilterableProductTable products={products} />;
 }
 
-export default MockUp
+export default MockUp;
